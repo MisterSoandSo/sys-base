7fad966e4c4396b49a448178d65499771c5f2081
MisterSoandSo <aso001@ucr.edu>
2015-04-10 19:57:31 -0700

    install script 1.00

---
+++ installvim.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Dependecy on git
+#Pull github repository that holds vimrc
+git clone https://github.com/aso001/vimrc.git
+
+#Moving .vimrc to local files and removing .git repo from local files
+cd vimrc
+mv .vimrc ~/
+rm -rf .git
+cd ..
+rm -r vimrc
+exit
