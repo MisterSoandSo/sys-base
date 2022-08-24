20772d31e233d3cb7ef71eeea98e19c843ddcde9
Andrew So <asoso221@gmail.com>
2022-08-24 13:10:08 -0700

    Added comments and extra local config options

--- Maintenance/custom_gitpush.sh
+++ Maintenance/custom_gitpush.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+#Uncomment if you want set local config for a particular repo
+#git config --local user.email ""
+#git config --local user.name ""
 
 #Your Github username ....
 USERNAME="";
 
 #Your Github password or personal access token
-#DO NOT PUSH this script on any repo! This meant for convience while working on remote.
+#DO NOT PUSH this script on any public repo! This meant for convenience while working on remote.
 PASSWORD="";
 
 #Parses the link from git remote -v
 variable=$(git remote -v | awk '/push/ {print $2}' )
 UPSTREAM=${variable:8}
 
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+#git clone https://username:password@github.com/username/repository.git
 git push https://$USERNAME:$PASSWORD@$UPSTREAM
 
 
