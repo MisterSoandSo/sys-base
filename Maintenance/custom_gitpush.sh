be712e9834bf2b0cf56069fcdd94d4f322537dbc
Andrew So <asoso221@gmail.com>
2022-08-27 17:44:26 -0700

    Used environment variables to pass on credentials

--- Maintenance/custom_gitpush.sh
+++ Maintenance/custom_gitpush.sh
@@ -11,6 +11,18 @@ USERNAME="";
 #DO NOT PUSH this script on any public repo! This meant for convenience while working on remote.
 PASSWORD="";
 
+#Script will exit if the either username or password is empty
+if [ -z "$USERNAME" ];
+then
+	if [ -z ${git_user+x} ]; then echo "git_user is unset"; exit; else USERNAME=$git_user; fi
+fi
+
+if [ -z "$PASSWORD" ];
+then
+	if [ -z ${git_pass+x} ]; then echo "git_pass is unset"; exit; else PASSWORD=$git_pass; fi
+fi
+
+
 #Parses the link from git remote -v
 variable=$(git remote -v | awk '/push/ {print $2}' )
 UPSTREAM=${variable:8}
